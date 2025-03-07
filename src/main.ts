9e32f65dccb2f5080a0e0cf797e0f9d6eae26340
Ruslan <onishruslan@yandex.ru>
2025-03-07 17:54:06 +0300

    + swagger

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app/app.module'
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger'
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule)
@@ -8,6 +9,15 @@ async function bootstrap() {
         methods: 'GET, POST, PUT, DELETE',
         allowedHeaders: 'Content-Type, Authorization'
     })
+
+    const config = new DocumentBuilder()
+        .setTitle('My API')
+        .setDescription('The API description')
+        .setVersion('1.0')
+        .addTag('auth') // Добавь нужные теги для твоего API
+        .build()
+    const document = SwaggerModule.createDocument(app, config)
+    SwaggerModule.setup('api', app, document) // Устанавливаем Swagger по пути /api
 
     app.setGlobalPrefix('api')
     await app.listen(process.env.PORT ?? 3000)
